5c21071e1be60949a744675e964769752e61e041
Vinnie Falco <vinnie.falco@gmail.com>
2023-07-03 21:41:03 -0700

    chore: javadoc work

--- test-files/adoc/test.cpp
+++ test-files/adoc/test.cpp
@@ -1,1 +1,28 @@
-struct T {};
+void f0();
+
+///
+void f1();
+
+/**
+*/
+void f2();
+
+/** 
+*/
+void f3();
+
+/** brief
+*/
+void f4();
+
+/** @brief
+*/
+void f5();
+
+/** @brief  
+*/
+void f6();
+
+/** @brief brief
+*/
+void f7();
